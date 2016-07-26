ad6b38873c2f81d1e3e5690be172df654aab3c79
g1nox <g1nox@192.168.0.6>
2016-07-26 01:36:31 -0300

    Actualización vistas

--- src/main/java/com/egs/webapp/managedBeans/DetalleController.java
+++ src/main/java/com/egs/webapp/managedBeans/DetalleController.java
@@ -1,6 +1,8 @@
 package com.egs.webapp.managedBeans;
 
-import com.egs.webapp.entities.Detallepedido;
+import com.egs.webapp.entities.DetallePedido;
+import com.egs.webapp.entities.Ingrediente;
+import com.egs.webapp.entities.Receta;
 import com.egs.webapp.sessionBeans.DetallepedidoFacade;
 import com.egs.webapp.util.JsfUtil;
 import java.io.Serializable;
@@ -34,18 +36,14 @@ public class DetalleController implements Serializable {
     /**
      * Creates a new instance of DetalleController
      */
-    
     @EJB
     private com.egs.webapp.sessionBeans.DetallepedidoFacade ejbFacade;
-    private List<Detallepedido> items = null;
-    private Detallepedido selected;
+    private List<DetallePedido> items = null;
+    private DetallePedido selected;
     // This items are only available in shopping cart 
-    private Detallepedido currentDetallePedido; 
-    private List<Detallepedido> currentItems = null;
-    
-    
-    
-    
+    private DetallePedido currentDetallePedido;
+    private List<DetallePedido> currentItems = null;
+
     @EJB
     private DetallepedidoFacade detallepedidoFacade;
 
@@ -56,10 +54,8 @@ public class DetalleController implements Serializable {
     public void setDetallepedidoFacade(DetallepedidoFacade detallepedidoFacade) {
         this.detallepedidoFacade = detallepedidoFacade;
     }
-    
-    
-    
-    @Inject    
+
+    @Inject
     private ProductoController currentproducto;
 
     public ProductoController getCurrentproducto() {
@@ -69,7 +65,7 @@ public class DetalleController implements Serializable {
     public void setCurrentproducto(ProductoController currentproducto) {
         this.currentproducto = currentproducto;
     }
-    
+
     @Inject
     private PedidoController currentpedido;
 
@@ -81,115 +77,195 @@ public class DetalleController implements Serializable {
         this.currentpedido = currentpedido;
     }
 
-    
-    public DetalleController() {
-        
+    @Inject
+    private IngredienteController currentingrediente;
+
+    public IngredienteController getCurrentingrediente() {
+        return currentingrediente;
     }
-        
+
+    public void setCurrentingrediente(IngredienteController currentingrediente) {
+        this.currentingrediente = currentingrediente;
+    }
+
+    @Inject
+    private RecetaController currentreceta;
+
+    public RecetaController getCurrentreceta() {
+        return currentreceta;
+    }
+
+    public void setCurrentreceta(RecetaController currentreceta) {
+        this.currentreceta = currentreceta;
+    }
+
+    public DetalleController() {
+
+    }
+
     public DetallepedidoFacade getEjbFacade() {
         return ejbFacade;
     }
-    
-    public List<Detallepedido> getItems() {
+
+    public List<DetallePedido> getItems() {
         if (items == null) {
             items = getEjbFacade().findAll();
         }
         return items;
     }
 
-    public void setCurrentItems(List<Detallepedido> currentItems) {
+    public void setCurrentItems(List<DetallePedido> currentItems) {
         this.currentItems = currentItems;
     }
-        
 
-    public Detallepedido getSelected() {
+    public DetallePedido getSelected() {
         return selected;
     }
-    
+
     protected void initializeEmbeddableKey() {
     }
-    
-    
-    public Detallepedido prepareCreate() {
-           
-      this.currentItems = new ArrayList<Detallepedido>();
-      this.currentDetallePedido = new Detallepedido();
-      
-      currentproducto.setSelectedProducto(null);
-     
-      return currentDetallePedido;
-    }
-    
-    public void init (){
-         
-        if (currentDetallePedido == null){
-         currentDetallePedido = new Detallepedido();
-         }
-         if (currentItems == null){
-         currentItems = new ArrayList <Detallepedido>();
-         }
-         
-     }
-    
-    public String reinit (){
-         currentItems = new ArrayList <Detallepedido>();
-         currentDetallePedido = new Detallepedido(); // setea el objeto detallepedido para usarlo de nuevo
-         currentproducto.setSelectedProducto(null); // setea el campo de producto del command button
-         
-         return null;
-     }
-    
-    public void rereinit(){
-        currentItems = new ArrayList <Detallepedido>();
-    
-    }
-     
-     
-    public void addShoppingCart() {
-     //   int stockActual = currentproducto.getSelectedProducto().getStockActual();
-        Date d = new Date();
-        int stockActual = currentproducto.getSelectedProducto().getStockActual();
-        
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-        
-        if(stockActual >= currentDetallePedido.getCantArt()){
-            currentproducto.getSelectedProducto().setStockActual(stockActual - currentDetallePedido.getCantArt());
-            currentproducto.actualizarStock();
-            currentDetallePedido.setHoraIng(sdf.format(d)); 
-            currentDetallePedido.setPrecioUni(currentproducto.getSelectedProducto().getPrecio_venta());
-            currentDetallePedido.setPrecioTotal(currentDetallePedido.getPrecioUni()*currentDetallePedido.getCantArt());
-            currentDetallePedido.setIdProducto(currentproducto.getSelectedProducto());
 
-            currentItems.add(currentDetallePedido);
-            currentDetallePedido = new Detallepedido();  
-            
-            
-        }else{
-         //chantarle un mensaje aqui de que falta stock
-           
-            JsfUtil.addErrorMessage("Stock insuficiente");
-        }
+    public DetallePedido prepareCreate() {
+
+        this.currentItems = new ArrayList<DetallePedido>();
+        this.currentDetallePedido = new DetallePedido();
+
+        currentproducto.setSelectedProducto(null);
+
+        return currentDetallePedido;
     }
+
+    public void init() {
+
+        if (currentDetallePedido == null) {
+            currentDetallePedido = new DetallePedido();
+        }
+        if (currentItems == null) {
+            currentItems = new ArrayList<DetallePedido>();
+        }
+
+    }
+
+    public String reinit() {
+        currentItems = new ArrayList<DetallePedido>();
+        currentDetallePedido = new DetallePedido(); // setea el objeto detallepedido para usarlo de nuevo
+        currentproducto.setSelectedProducto(null); // setea el campo de producto del command button
+
+        return null;
+    }
+
+    public void rereinit() {
+        currentItems = new ArrayList<DetallePedido>();
+
+    }
+
+    public void addShoppingCart() {
+
+        if (currentproducto.getSelectedProducto().getCompuesto() == false) {
+
+            int stockActual = currentproducto.getSelectedProducto().getStockActual();
+
+            if (stockActual >= currentDetallePedido.getCantArt()) {
+
+                currentproducto.getSelectedProducto().setStockActual(stockActual - currentDetallePedido.getCantArt());
+                currentproducto.actualizarStock();
+
+                //define hora de ingreso
+                Date d = new Date();
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+                currentDetallePedido.setHoraIng(sdf.format(d));
+
+                currentDetallePedido.setPrecioUni(currentproducto.getSelectedProducto().getPrecioVenta());
+                currentDetallePedido.setPrecioTotal(currentDetallePedido.getPrecioUni() * currentDetallePedido.getCantArt());
+                currentDetallePedido.setIdProducto(currentproducto.getSelectedProducto());
+
+                currentItems.add(currentDetallePedido);
+                currentDetallePedido = new DetallePedido();
+                
+                //deja null el producto despues de agregarse al pedido
+                currentproducto.setSelectedProducto(null);
+
+            } else {
+                //chantarle un mensaje aqui de que falta stock
+
+                JsfUtil.addErrorMessage("Stock insuficiente");
+            }
+        } else {
+
+            // le pasa el id del producto seleccionado
+            int idProducto = currentproducto.getSelectedProducto().getIdProducto();
+
+            List<Receta> recetas = currentreceta.getRecetasPorProducto(idProducto);
+            ArrayList<Ingrediente> ingredientesSinStock = new ArrayList<Ingrediente>();
+            ArrayList<Ingrediente> ingredientesConStock = new ArrayList<Ingrediente>();
+            for (Receta receta : recetas) {
+                double stockActual = receta.getIdIngrediente().getStockActual();
+
+                if (stockActual < receta.getCantidad()) {
+
+                    ingredientesSinStock.add(receta.getIdIngrediente());
+
+                } else {
+                    ingredientesConStock.add(receta.getIdIngrediente());
+                }
+
+            }
+
+            if (ingredientesSinStock.size() > 0) {
+                String mensaje = "";
+                for(Ingrediente ingrediente : ingredientesSinStock ){
+                 mensaje += "\n "+ ingrediente.getNombre();
+                
+                }
+                JsfUtil.addSuccessMessage("Los siguientes productos no poseen stock"+mensaje);
+                
+            } else {
+                for (Receta receta : recetas) {
+                    double stockActual = receta.getIdIngrediente().getStockActual();
+                    double cantidad = receta.getCantidad();
+
+                    receta.getIdIngrediente().setStockActual(stockActual - cantidad);
+                    currentingrediente.setSelected(receta.getIdIngrediente());
+                    currentingrediente.actualizarStock();
+
+                }
+                    //define hora de ingreso
+                    Date d = new Date();
+                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+                    currentDetallePedido.setHoraIng(sdf.format(d));
+
+                    currentDetallePedido.setPrecioUni(currentproducto.getSelectedProducto().getPrecioVenta());
+                    currentDetallePedido.setPrecioTotal(currentDetallePedido.getPrecioUni() * currentDetallePedido.getCantArt());
+                    currentDetallePedido.setIdProducto(currentproducto.getSelectedProducto());
+
+                    currentItems.add(currentDetallePedido);
+                    currentDetallePedido = new DetallePedido();
+                
+            }
+
+            
+        }
+
+    }
+
     public void removeShoppingCart() {
         int stockActual = currentproducto.getSelectedProducto().getStockActual();
-        
+
         currentproducto.getSelectedProducto().setStockActual(stockActual + currentDetallePedido.getCantArt());
-        currentproducto.actualizarStock();       
-       
+        currentproducto.actualizarStock();
+
     }
-    
-    
-    
+
     public String create() {
-        persist(JsfUtil.PersistAction.CREATE,  "detalle creado");
+        persist(JsfUtil.PersistAction.CREATE, "detalle creado");
         if (!JsfUtil.isValidationFailed()) {
             items = null;    // Invalidate list of items to trigger re-query.
             FacesContext facesContext = FacesContext.getCurrentInstance();
-             Flash flash = facesContext.getExternalContext().getFlash();
-             flash.setKeepMessages(true);
-             flash.setRedirect(true);
-             prepareCreate();
-             return goDetalleCreate();
+            Flash flash = facesContext.getExternalContext().getFlash();
+            flash.setKeepMessages(true);
+            flash.setRedirect(true);
+            prepareCreate();
+            return goDetalleCreate();
         }
         return null;
     }
@@ -205,23 +281,21 @@ public class DetalleController implements Serializable {
             items = null;    // Invalidate list of items to trigger re-query.
         }
     }
-    
- private void persist(JsfUtil.PersistAction persistAction, String successMessage) {
+
+    private void persist(JsfUtil.PersistAction persistAction, String successMessage) {
         if (currentDetallePedido != null) {
-           
+
             try {
                 if (persistAction != JsfUtil.PersistAction.DELETE) {
 //                    Date d = new Date();
 //                    currentpedido.setFecha(d);
 //                    currentpedido.setHora(d);
-                    
-                  
-                   
+
                     getEjbFacade().edit(currentDetallePedido);
                 } else {
-                   getEjbFacade().remove(currentDetallePedido);
-               }
-            
+                    getEjbFacade().remove(currentDetallePedido);
+                }
+
             } catch (EJBException ex) {
                 String msg = "";
                 Throwable cause = ex.getCause();
@@ -239,21 +313,20 @@ public class DetalleController implements Serializable {
             }
         }
     }
-    
-    
-    public Detallepedido getDetallepedido(java.lang.Long id) {
+
+    public DetallePedido getDetallepedido(java.lang.Long id) {
         return getEjbFacade().find(id);
     }
 
-    public List<Detallepedido> getItemsAvailableSelectMany() {
+    public List<DetallePedido> getItemsAvailableSelectMany() {
         return getEjbFacade().findAll();
     }
 
-    public List<Detallepedido> getItemsAvailableSelectOne() {
+    public List<DetallePedido> getItemsAvailableSelectOne() {
         return getEjbFacade().findAll();
     }
 
-    @FacesConverter(forClass = Detallepedido.class)
+    @FacesConverter(forClass = DetallePedido.class)
     public static class DetalleControllerConverter implements Converter {
 
         @Override
@@ -266,9 +339,9 @@ public class DetalleController implements Serializable {
             return controller.getProducto(getKey(value));
         }
 
-        java.lang.Long getKey(String value) {
-            java.lang.Long key;
-            key = Long.valueOf(value);
+        java.lang.Integer getKey(String value) {
+            java.lang.Integer key;
+            key = Integer.valueOf(value);
             return key;
         }
 
@@ -283,63 +356,62 @@ public class DetalleController implements Serializable {
             if (object == null) {
                 return null;
             }
-            if (object instanceof Detallepedido) {
-                Detallepedido o = (Detallepedido) object;
+            if (object instanceof DetallePedido) {
+                DetallePedido o = (DetallePedido) object;
                 return getStringKey(o.getIdDetalle());
             } else {
-                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, "object {0} is of type {1}; expected type: {2}", new Object[]{object, object.getClass().getName(), Detallepedido.class.getName()});
+                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, "object {0} is of type {1}; expected type: {2}", new Object[]{object, object.getClass().getName(), DetallePedido.class.getName()});
                 return null;
             }
         }
 
     }
 
-    public Detallepedido getCurrentDetallePedido() {
+    public DetallePedido getCurrentDetallePedido() {
         return currentDetallePedido;
     }
 
-    public List<Detallepedido> getCurrentItems() {
+    public List<DetallePedido> getCurrentItems() {
         return currentItems;
     }
-   
-    public String goDetalleCreate(){
-    prepareCreate();
-    return "pedido-create";
+
+    public String goDetalleCreate() {
+        prepareCreate();
+        return "pedido-create";
     }
-    
-    public String goDetalleList(){
-    prepareCreate();
-    return "pedido-list";
+
+    public String goDetalleList() {
+        prepareCreate();
+        return "pedido-list";
     }
-    
-    public String goDetalleEdit(){
-    prepareCreate();
-    return "pedido-update";
-   }
-    
-   
 
-      public void llamarView() {
+    public String goDetalleEdit() {
+        prepareCreate();
+        return "pedido-update";
+    }
 
-        if ( currentpedido != null)  {
+    public void llamarView() {
 
-                 } else{JsfUtil.addErrorMessage("Error");}
-            }
-    
-       public void viewDetalle(Detallepedido currentDetallepedido) {
         if (currentpedido != null) {
-            
+
+        } else {
+            JsfUtil.addErrorMessage("Error");
+        }
+    }
+
+    public void viewDetalle(DetallePedido currentDetallepedido) {
+        if (currentpedido != null) {
+
             try {
                 getDetallepedidoFacade().edit(currentDetallepedido);
                 FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "PrimeFaces Rocks."));
-            } 
-             catch (Exception ex) {
+            } catch (Exception ex) {
                 Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
                 JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
             }
         }
-   }
-    
+    }
+
 }
           
     
